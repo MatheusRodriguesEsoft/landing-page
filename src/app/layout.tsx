8c146106a13992ac15eb58a95dcb146fb2498052
Matheus Rodrigues <matheus.rodrigues.esoft@gmail.com>
2025-03-18 11:13:19 -0300

    fix-metadata-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
-export const interTight = Inter_Tight({
+const interTight = Inter_Tight({
   variable: "--font-inter-tight",
   subsets: ["latin"],
 });

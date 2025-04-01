18160314b44231ea25202d42efcbe0c637a3c264
Matheus Rodrigues <matheus.rodrigues.esoft@gmail.com>
2025-04-01 11:30:06 -0300

    update-final-version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Menu } from "./Menu";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="w-full h-[110.5px] flex items-center justify-center  bg-primary-base dark:bg-elevated dark:text-white fixed">
       <div className="w-full max-w-[1440px] flex items-center justify-between px-[20px] py-[40px] md:px-[80px] lg:px-[120px]">
-        <div className="flex items-center h-12">
+        <Link href={'https://landing-page-opal-one-94.vercel.app/'} className="flex items-center h-12">
           <Image
             className="w-48 h-auto hidden md:block"
             src={"/images/logo.png"}
@@ -20,7 +21,7 @@ export const Header = () => {
             width={500}
             height={500}
           />
-        </div>
+        </Link>
         <div className="lg:w-[592px] h-full flex md:w-[480px] items-center justify-end ">
           <Menu />
         </div>
